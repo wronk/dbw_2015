ad407c1ee64fd30f22532ccf05907f3d6b67bd92
Rich Pang <rpang.contact@gmail.com>
2015-07-13 16:28:55 -0700

    Add tutorial directory with some scripts and notebooks.

---
+++ tutorial_python/scripts/example_script.py
@@ -0,0 +1,9 @@
+"""
+This script prints out a poem that has changed the lives of many. 
+"""
+print "A wonderful bird is the pelican."
+print "His bill can hold more than his beli-can."
+print "He can take in his beak"
+print "Food enough for a week"
+print "But I'm damned if I see how the heli-can."
+
